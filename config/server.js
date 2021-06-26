75972ccebd4f00ae47b33dc4095f43dc53b928fe
root <root@LAPTOP-HTSO6V7U.localdomain>
2021-06-26 13:29:07 +0530

    strapi backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bee5a1244eb13601a9015a18ee3c244d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a9f0eade-c36a-46d6-be2b-4afe823c2586'
+};
